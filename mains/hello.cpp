38b72501e482585908b239d539731bab0c7baf5f
Arley Prince Ossere Ebindza <ossereeb1u@etu.univ-lorraine.fr>
2022-09-26 16:16:54 +0200

    data for the test

---
+++ mains/hello.cpp
@@ -0,0 +1,8 @@
+#include <log_lib/print_log.h>
+
+int main()
+{
+	hello_world();
+	print_version();
+    return 0;
+}
---
+++ script.bat
@@ -0,0 +1,7 @@
+mkdir /s /q build
+mkdir build
+cd build
+cmake -DFIRSTORGANIZEDCMAKE_WITH_TESTS:BOOL=TRUE ..
+cmake --build . --config Release
+ctest -C Release
+
